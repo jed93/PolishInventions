5a9b67b447ac30a93ecd4646fbfd18b7935ee1d3
Jed93 <jedrekcebrat@gmail.com>
2017-06-08 19:43:24 +0200

    Zadanie 4 oraz 5

---
+++ PolishInventions_CSharp/Invention.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+
+namespace PolishInventions_CSharp
+{
+    public class Invention
+    {
+        public string ApplicationNumber { get; set; }
+        public string ExclusiveRightNumber { get; set; }
+        public string PriorityDate { get; set; }
+        public string ApplicationDate { get; set; }
+        public string BupNumber { get; set; }
+        public string BupPublicationDate { get; set; }
+        public string WupNumber { get; set; }
+        public string WupPublicationDate { get; set; }
+        public string Title { get; set; }
+        public string AuthorisedPeople { get; set; }
+        public string Creators { get; set; }
+        public string Proxies { get; set; }
+        public string MkpClassification { get; set; }
+        public string CaseStatus { get; set; }
+    }
+
+    public sealed class InventionMap : CsvClassMap<Invention>
+    {
+        public InventionMap()
+        {
+            Map(m => m.ApplicationNumber).Name("Numer_zgloszenia");
+            Map(m => m.ExclusiveRightNumber).Name("Numer_prawa_wylacznego");
+            Map(m => m.PriorityDate).Name("Data_pierwszenstwa");
+            Map(m => m.ApplicationDate).Name("Data_zgloszenia");
+            Map(m => m.BupNumber).Name("Numer_BUP");
+            Map(m => m.BupPublicationDate).Name("Data_Publikacji_BUP");
+            Map(m => m.WupNumber).Name("Numer_WUP");
+            Map(m => m.WupPublicationDate).Name("Data_Publikacji_WUP");
+            Map(m => m.Title).Name("Tytul");
+            Map(m => m.AuthorisedPeople).Name("Uprawnieni");
+            Map(m => m.Creators).Name("Tworcy");
+            Map(m => m.Proxies).Name("Pelnomocnicy");
+            Map(m => m.MkpClassification).Name("Klasyfikacja_MKP");
+            Map(m => m.CaseStatus).Name("Status_sprawy");
+        }
+    }
+}
---
+++ PolishInventions_CSharp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using CsvHelper;
+
+namespace PolishInventions_CSharp
+{
+    class Program
+    {
+        private static string csvFileName = "20170531listaWynalazkow.csv";
+
+        static IEnumerable<Invention> GetAllInventions(StreamReader thisStreamReader)
+        {
+            var csv = new CsvReader(thisStreamReader);
+            csv.Configuration.Delimiter = ";";
+            csv.Configuration.RegisterClassMap<InventionMap>();
+            var records = csv.GetRecords<Invention>();
+
+            //foreach (Invention record in records.Take(5))
+            //{
+            //    Debug.Print("{0} {1}, {2}", record.ApplicationNumber, record.ApplicationDate, record.Title);
+            //}
+
+            return records;
+        }
+
+        static void Main(string[] args)
+        {
+            char inputKey;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Witamy w programie. Wybierz pozycję menu: ");
+                Console.WriteLine("1. Zwróć trzy wynalazki, których tytuł rozpoczyna się od S lub P.");
+                Console.WriteLine("2. Zwróć liczbę wynalazków zgłoszonych w styczniu '89.");
+                Console.WriteLine("0. Zakończ pracę programu.");
+                Console.WriteLine();
+                Console.Write("Twój wybór: ");
+
+                inputKey = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (inputKey)
+                {
+                    case '1':
+                        using (StreamReader thisStreamReader = new StreamReader(csvFileName))
+                        {
+                            var particularInventions = GetAllInventions(thisStreamReader)
+                                .Where(i => i.Title.StartsWith("S") || i.Title.StartsWith("P")).Take(3);
+                            foreach (var invention in particularInventions)
+                            {
+                                Console.WriteLine("* {0}: {1}, {2}; twórcy: {3}", invention.ApplicationDate,
+                                    invention.ApplicationNumber, invention.Title, invention.Creators);
+                            }
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Naciśnij dowolny klawisz, aby kontynować.");
+                        Console.ReadKey();
+                        break;
+                    case '2':
+                        using (StreamReader thisStreamReader = new StreamReader(csvFileName))
+                        {
+                            var inventionsAmount = GetAllInventions(thisStreamReader)
+                                .Where(i => i.ApplicationDate.StartsWith("1989-01")).Count();
+                            Console.WriteLine("Ilość zgłoszonych wynalazków w styczniu 1989 roku: " + inventionsAmount);
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Naciśnij dowolny klawisz, aby kontynować.");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
+                }
+            } while (inputKey != '0');
+           
+        }
+    }
+}
